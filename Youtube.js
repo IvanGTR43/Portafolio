94c7f0934cd3caa3d93b778ae064bac1959addcf
IvanGTR <ivan_tr43@outlook.com>
2020-11-26 13:25:02 -0600

    Mas Archivos

---
+++ Youtube.js
@@ -0,0 +1,13 @@
+
+verdadero = true;
+unde = undefined;
+const nombre = "Ivan";
+let numero;
+numero = 29
+numero = 22
+console.log(unde)
+function numeros(var1, var2){
+    return var1 + var2
+}
+console.log(numeros(1,1))
+
